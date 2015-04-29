72d0840769e7e02190310fb1029edbe622ab2317
Vincent <ghzmdr@gmail.com>
2015-04-29 17:56:24 +0200

    update

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var dgram = require('dgram'),
     server = dgram.createSocket('udp4'),
     dns = require('dns')
 
-//var url = 'vincentdefeo.me',
-//    port = 8080
+var url = 'vincentdefeo.me',
+   port = 8080
 
-/*dns.lookup(url, function resolved(err, addresses){
+dns.lookup(url, function resolved(err, addresses){
     if (err) throw err
     console.log(url + ' RESOLVED TO: ' + addresses)
-})*/
+    server.bind(38359, addresses)
+})
 
-server.bind('127.0.0.1', 38359)
 
 server.on('message', function (msg, r){
     console.log(r.address + ':' + r.port + ' -> ' + message)
