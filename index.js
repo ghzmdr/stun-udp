6cab9eec5ce21b2f364272ea7134c10c6227d49f
Vincent <ghzmdr@gmail.com>
2015-04-30 13:12:34 +0200

    keypress sending

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var dgram = require('dgram'),
     server = dgram.createSocket('udp4'),
-    dns = require('dns')
+    dns = require('dns'),
+    keypress = require('keypress')
 
 var url = 'vincentdefeo.me',
    port = 8080
+
+var clients = []
+
+keypress(process.stdin)
+
+process.stdin.on('keypress', function (ch, key) {
+    console.log(clients)
+    
+    for (var i = 0; i < clients.length; i++)
+        server.send('data from netherlands', 0, clients[i].port, clients[i].addresses)
+});
+
 
 dns.lookup(url, function resolved(err, addresses){
     if (err) throw err
@@ -19,7 +32,8 @@ server.on('listening', function(){
 server.on('message', function (msg, r){
     console.log('\nGOT MESSAGE:\n' + msg + '\nFROM: ' + r.address + ':' + r.port)
     console.log('\nON: ' + Date.now())
-    server.send(msg, 0, msg.length, r.port, r.address)
+    server.send(msg, 0, msg.length, r.port, r.address)    
+    clients.push({'ip': r.address, 'port' : r.port})
 })
 
 server.on('error', function (err){
