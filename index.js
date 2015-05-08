ede27142fedaa5be72724a7af7ca17caf44b2487
Vincent <ghzmdr@gmail.com>
2015-05-08 20:37:06 +0200

    update

--- index.js
+++ index.js
@@ -8,81 +8,99 @@ var url = 'vincentdefeo.me',
 
 var clients = []
 
+var RIGHT_HUE = 200, LEFT_HUE = 100
+
+var sendInterval = false;
+
+
 keypress(process.stdin)
 process.stdin.setRawMode(true)
 
 process.stdin.on('keypress', function(c, k) {
-    if (c == 'c' || k == 'c')
+    if (c == 'q' || k == 'q')
         process.exit(0)
 
-    else if (c == '\'' || k == '\''){
-        console.log("\nGot Keypress\n")
-
-        for (var i = 0; i < clients.length; i++)  
-        {
-            clients[i].packetsSent++
-            clients[i].toggle = !clients[i].toggle
-
-            //var msg = new Buffer(JSON.stringify(clients[i]))
-            lCol = 100//Math.floor(Math.random()*360 +1)
-            rCol = 200//Math.floor(Math.random()*360 +1)
-            
-            var msg = new Buffer(JSON.stringify({
-                        hue: {
-                            right: rCol,
-                            left: lCol
-                        },  
-                        saturation: 100,
-                        lightness: 50
-                    }))
-            server.send(msg, 0, msg.length, clients[i].port, clients[i].address)    
-            console.log("SENDING TO: ", clients[i].address + ' ' + clients[i].port)
-            console.log("HUES: R = " + rCol + " L = " + lCol)
-        }
+    else if (c == 's' || k == 's') {
+        console.log("\nSending responses\n")
+        sendPackets()        
     }
 
-    else if (c == '1' || k == '1')
-    {
-        console.log(clients)
+    else if (c == 'r' || k == 'r') {
+        console.log("\nSending responses with randomized hues\n")
+        sendPackets(true)        
+    }
+
+    else if (c == 'l' || k == 'l') {
+        console.log("\n\n==== CLIENTS ====\n\n", clients)
+    }
+
+    else if (c == '0' || k == '0') {
+        if (!sendInterval) 
+            sendInterval = setInterval(sendPackets, 1000)        
+    }
+
+    else if (c == '9' || k == '9') {
+        if (sendInterval)
+            clearInterval(sendInterval)
     }
 })
 
-/*
-setInterval(function() {
-	for (var i = 0; i < clients.length; i++)
-        {
-            clients[i].packetsSent++
-            clients[i].toggle = !clients[i].toggle
+function sendPackets(random, singleHost) {
+    console.log("\nSENDING " + (random ? "RADOMIZED" : "") + "PACKETS TO " + (singleHost ? "1 CLIENT" : clients.length + " CLIENTS"))
 
-            //var msg = new Buffer(JSON.stringify(clients[i]))
-            lCol = 100//Math.floor(Math.random()*360 +1)
-            rCol = 200//Math.floor(Math.random()*360 +1)
+    var msg = craftPacket(random)
 
-            var msg = new Buffer(JSON.stringify({
-                        hue: {
-                            right: rCol,
-                            left: lCol
-                        },
-                        saturation: 100,
-                        lightness: 50
-                    }))
-            server.send(msg, 0, msg.length, clients[i].port, clients[i].address)
-            console.log("SENDING TO: ", clients[i].address + ' ' + clients[i].port)
-            console.log("HUES: R = " + rCol + " L = " + lCol)
+    if (singleHost){
+        if (singleHost.packetsSent)
+            singleHost.packetsSent++
+
+        server.send(msg, 0, msg.length, singleHost.port, singleHost.address)
+    }
+
+    else for (var i = 0; i < clients.length; i++) {
+        clients[i].packetsSent++
+        clients[i].toggle = !clients[i].toggle                
+
+        server.send(msg, 0, msg.length, clients[i].port, clients[i].address)  
+    }
+}
+
+function craftPacket(random) {
+    rCol = random ? Math.floor(Math.random()*360 +1) : RIGHT_HUE
+    lCol = random ? Math.floor(Math.random()*360 +1) : LEFT_HUE
+
+    var payloadObj = {
+            hue: {
+                right: rCol,
+                left: lCol
+            },  
+            saturation: 100,
+            lightness: 50
         }
-}, 300)
-*/
 
+    return new Buffer(JSON.stringify(payloadObj))
+}
+
+function printUsage() {
+    console.log('\n\n====== USAGE =====\n')
+    console.log("\tQ\t--\tQuit (also deletes clients cache)")
+    console.log("\tR\t--\tSend packet with randomized hues")
+    console.log("\tS\t--\tSend packet standard hues (R: 100, L:200)")
+    console.log("\tL\t--\tDisplay known clients")
+    console.log("\t0\t--\tStart sending packets every 1000ms")
+    console.log("\t9\t--\tStop sending packets every 1000ms")
+}
 
 dns.lookup(url, function resolved(err, addresses){
     if (err) throw err
     console.log('\nSTARTING...\n' + url + ' RESOLVED TO: ' + addresses)
     server.bind(port, addresses)
+    printUsage()
 })
 
 server.on('listening', function(){
     var address = server.address()
-    console.log('Listening on : ' + address.address + ':' + address.port)
+    console.log('\n\nListening on : ' + address.address + ':' + address.port)    
 })
 
 server.on('message', function (msg, r){    
@@ -114,20 +132,7 @@ server.on('message', function (msg, r){
     console.log('ON: ' + Date.now())
     console.log('\n===PAYLOAD:\n' + msg)
 
-    //var resp = new 
-    Buffer(JSON.stringify(clientInfo || clients[index]) + '\n\n')
-    lCol = 100//Math.floor(Math.random()*360 +1)
-    rCol = 200//Math.floor(Math.random()*360 +1)
-    var resp = new Buffer(JSON.stringify({
-        hue: {
-            right: rCol,
-            left: lCol
-        },  
-        saturation: 100,
-        lightness: 50
-    }))
-
-    server.send(resp, 0, resp.length, r.port, r.address)    
+    sendPackets(false, {port: r.port, address: r.address})
 })
 
 server.on('error', function (err){
